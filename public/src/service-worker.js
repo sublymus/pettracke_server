030e683f50a5e9c56ee5236b0e6b17498a4daf61
sublymus <sublymus@gmail.com>
2024-09-18 10:27:33 +0300

    lola

--- public/src/service-worker.js
+++ public/src/service-worker.js
@@ -15,3 +15,5 @@ self.addEventListener('install', (event) => {
       })
     );
   });
+
+  
--- public/worker.js
+++ public/worker.js
@@ -4,7 +4,37 @@ self.addEventListener('push', e => {
     console.log("Push Received...");
     self.registration.showNotification(data.title, {
         body: data.content,
-        icon: 'https://react-leaflet.js.org/img/logo.svg'
+        sound: 'http://localhost:3333/src/res/level-up-191997.mp3',
+        icon: '/src/res/dog-area.png',
+        actions: [
+            {
+                action: 'open',
+                title: 'Open App'
+            }
+        ]
     });
     console.log(e);
+});
+
+self.addEventListener('notificationclick', (event) => {
+
+    console.log('notifaction ==> ', event);
+    event.notification.close();
+
+    event.waitUntil(
+        clients.matchAll({ type: 'window' }).then((clientList) => {
+
+            console.log('clientList ==> ', clientList);
+            if (clientList.length > 0) {
+                // Si une fenêtre est ouverte, joue le son
+                clientList[0].focus();
+                clientList[0].postMessage({ action: 'playSound' });
+            } else {
+                // Sinon, ouvre une nouvelle fenêtre
+                clients.openWindow('/').then((windowClient) => {
+                    windowClient.postMessage({ action: 'playSound' });
+                });
+            }
+        })
+    );
 });
