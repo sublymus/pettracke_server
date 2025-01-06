ef9bad827dc66e72c0b83db0927eb48f2c1b268c
sublymus <sublymus@gmail.com>
2025-01-06 12:50:17 +0300

    lodv

--- start/env.ts
+++ start/env.ts
@@ -12,23 +12,7 @@
 import { Env } from '@adonisjs/core/env'
 
 export default await Env.create(new URL('../', import.meta.url), {
-  NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
-  PORT: Env.schema.number(),
-  APP_KEY: Env.schema.string(),
-  HOST: Env.schema.string({ format: 'host' }),
-  LOG_LEVEL: Env.schema.enum(['fatal', 'error', 'warn', 'info', 'debug', 'trace']),
-  CALL_BACK_URL:Env.schema.string(),
-  // DIR_NAME : Env.schema.string(),
-  //////////////////  My ENV
-  FRONT_ORIGINE: Env.schema.string(),
-  FRONT_END_AUTH: Env.schema.string(),
-  FRONT_END_REGISTER: Env.schema.string(),
-  GOOGLE_CALLBACK: Env.schema.string(),
-  GOOGLE: Env.schema.string(),
-  DEFAULT_LIMIT: Env.schema.string(),
-  FILE_STORAGE_PATH: Env.schema.string(),
-  FILE_STORAGE_URL: Env.schema.string(),
-  PUBLIC_PATH: Env.schema.string(),
+  
   /*
   |----------------------------------------------------------
   | Variables for configuring ally package
